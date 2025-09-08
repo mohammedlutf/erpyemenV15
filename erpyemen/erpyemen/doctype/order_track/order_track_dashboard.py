69639c63bf1e2462e737c06d528b02474f4530a5
Mohammed lutf <m.lutfdalal@gmail.com>
2025-09-08 22:42:29 +0300

    add order track dashboard

---
+++ erpyemen/erpyemen/doctype/order_track/order_track_dashboard.py
@@ -0,0 +1,14 @@
+from frappe import _
+
+
+def get_data():
+	return {
+		"heatmap": True,
+		"heatmap_message": _("This is based on the Time Sheets created against this project"),
+		"fieldname": "order_track",
+		"transactions": [
+			
+			{"label": _("Sales"), "items": ["Sales Order", "Delivery Note", "Sales Invoice"]},
+			{"label": _("Purchase"), "items": ["Purchase Order", "Purchase Receipt", "Purchase Invoice"]},
+		],
+	}
